31ca722419180c0b83d5981737cae23a665b31eb
Andrea055 <vivanco11.ac@gmail.com>
2021-08-17 15:46:51 +0200

    add ace

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ sudo apt-key add archive-key.asc
 sudo apt update
 sudo sh -c "echo 'Package: *'>/etc/apt/preferences.d/kali.pref; echo 'Pin: release a=kali-rolling'>>/etc/apt/preferences.d/kali.pref; echo 'Pin-Priority: 50'>>/etc/apt/preferences.d/kali.pref"
 sudo apt update
+sudo apt install ace
 sudo apt install dmitry
 sudo apt install ike-scan
 sudo apt install legion
