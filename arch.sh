7d245ee02cf35ada8bd2953f1c4a82fc1a1f0277
Nusk-Rbb <nuskrbb1741dn@gmail.com>
2024-09-25 07:12:10 +0900

    update: remove whoami

--- arch.sh
+++ arch.sh
@@ -17,7 +17,6 @@ function install_metasploit() {
 function add_repos() {
     echo "Installing blackarch repo..."
     curl -O https://blackarch.org/strap.sh
-    whoami
     sudo sh -c "$(pwd)/strap.sh"
 }
 
